c134caf9b75820cd0ddcc33ac0abcb76ea0375f4
Artem Petrov <artem.petrov@umbrellait.com>
2022-12-12 18:38:31 +0300

    Fix install for linux

--- ares_install.py
+++ ares_install.py
@@ -6,6 +6,8 @@ import stat
 
 ARES_HOME = os.path.expanduser('~/.ares')
 BASH_PROFILE_PATH = os.path.expanduser('~/.bash_profile')
+PROFILE_PATH = os.path.expanduser('~/.profile')
+BASHRC_PATH = os.path.expanduser('~/.bashrc')
 ZSHRC_PATH = os.path.expanduser('~/.zshrc')
 
 
@@ -20,6 +22,14 @@ def main():
     if os.path.exists(BASH_PROFILE_PATH):
         with open(BASH_PROFILE_PATH, mode='a') as bash_profile:
             bash_profile.write('\nexport PATH=${PATH}:/Users/$USER/.ares\n')
+
+    if os.path.exists(PROFILE_PATH):
+        with open(PROFILE_PATH, mode='a') as profile:
+            profile.write('\nexport PATH=${PATH}:/Users/$USER/.ares\n')
+
+    if os.path.exists(BASHRC_PATH):
+        with open(BASHRC_PATH, mode='a') as bashrc:
+            bashrc.write('\nexport PATH=${PATH}:/Users/$USER/.ares\n')
 
     if os.path.exists(ZSHRC_PATH):
         with open(ZSHRC_PATH, mode='a') as zshrc:
